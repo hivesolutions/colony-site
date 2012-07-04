3c036d54d769e0a2addf22f2ee8b5b06e73f979b
João Magalhães <joamag@gmail.com>
2012-07-04 16:33:02 +0100

    new code refactor in the plugins

--- src/colony_site_plugin.py
+++ src/colony_site_plugin.py
@@ -50,7 +50,6 @@ class ColonySitePlugin(colony.base.system.Plugin):
     description = "The plugin that offers the colony web site"
     version = "1.0.0"
     author = "Hive Solutions Lda. <development@hive.pt>"
-    loading_type = colony.base.system.EAGER_LOADING_TYPE
     platforms = [
         colony.base.system.CPYTHON_ENVIRONMENT
     ]
